8a4680e11891f2a2592b2e9619f17e0829c99978
al-Anqa <ahmed.m.almousawi@gmail.com>
2022-05-20 18:19:43 -0600

    Re-added iGEMScraper files

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# iGEMScraper
+# SAMARA iGEM Research Assistant
+## iGEMScraper
+
+A web scraper created using python and scrapy in order to automatically visit, parse, and extract information from iGEM wiki pages. The scraper
+was designed as the first component of the larger SAMARA Research Assistant project. However, in order to keep it accessible to other teams, it
+has also been released as a standalone module, allowing other teams to  easily scrape iGEM wiki pages using their own parameters.
+
+### Installation
+
+1. Download the files from the repository
+2. Create a virtual environment and run the following command from inside the virtual environment to install the required packages:  
+`pip install -r requirements.txt`  
+  
+A brief overview of the packages installed and their usage in the progream are listed below:
+
+| Library/Module | Usage |
+| -------------- | ----- |
+| Scrapy | Creates the CrawlSpider used to parse and scrape iGEM wiki pages. |
+| RegEx | Allows for string processing and the usage of wildcard text patterns. |
+| lxml | Allows for HTML processing and cleaning to help sanitize scraping outcomes. |
+| parsel | Adds CSS selectors to extract text from the html page body. |
+
+## Usage
+
+The program is run through the terminal. In order to do so, we must first navigate to the first netscrape_nav folder. If inside the project folder, the command to do so is as follows:
+  
+`cd ./netscrape_nav`  
+  
+We can then run the scraper itself using the following command:  
+  
+`scrapy crawl tomholland -O samara.csv --logfile scraper.log`  
+
+This command can be modified to suit your needs and documentation to do so is available on the [Scrapy docs](https://docs.scrapy.org/en/latest/index.html). By default, the program is set to overwrite the samara.csv file. Supported filetypes and storage options can also be found in the Scrapy documentation. CSV files, while smaller in size than the alternatives, do come with some issues. Namely, the presence of commas in the extracted text annoys CSV editing software. However, slight post-processing should be sufficient to overcome these obstacles.
+
+## Modification
+  
+As a default, the program scrapes any page that contains Software or Mode* in the URL. To change the pages scraped, you must first create a Link Extractor rule to parse the URL for certain keywords. Certain pages are standardized amongst iGEM teams and thus, are simpler to create rules for. Regular Expressions may be used to create wildcard rules to help increase the scope of the scraper for more inconsistant page namings(see rule #2 and the [RegularExpressions documentation](https://docs.python.org/3/library/re.html) for more information and examples of their usage). 
+
+
---
+++ netscrape_nav/netscrape_nav/spiders/iGEMscraper.py
@@ -0,0 +1,238 @@
+'''
+SAMARA iGEM Research Assistant
+iGEMScraper.py
+
+A web scraper created using python and scrapy in order to automatically visit, parse, and extract 
+information from iGEM wiki pages. In order to run, run the following commands from the terminal while
+the terminal directory points to the project directory:
+
+cd ./netscrape_nav
+
+scrapy crawl tomholland -O samara.csv --logfile scraper.log
+
+The second command can be changed into a variety of file formats, depending on your needs.
+See https://docs.scrapy.org/en/latest/topics/feed-exports.html for more information.
+
+Created by Ahmed Almousawi for the 2022 iGEM Calgary Team
+For inquiries or questions, email igemcalgary@ucalgary.ca or ahmed.almousawi1@ucalgary.ca
+
+Requried Packages:
+scrapy
+re
+lxml
+parsel
+'''
+
+from scrapy.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+import re
+from lxml import html
+from lxml.html.clean import Cleaner
+from parsel import Selector
+
+
+def getTeamname(url):
+    '''
+    Extracts the Team name from the url of an iGEM wiki page
+    
+    Arguments:
+        url (str): the url of a wiki page
+
+    Returns:
+        teamname (str): the name of the team that created the page
+    '''
+    
+    teamname = re.search('Team:(.+?)/', url).group()    # Extracts the teamname from the url
+    teamname = teamname.replace('Team:', '')    # Cleans the fluff from the extracted name
+    teamname = teamname.replace('/', '')        # Remove the / at the end
+    teamname = teamname.replace('_', ' ')       # Replace '_' with a ' '
+
+    return teamname
+
+def getYear(url):
+    '''
+    Extracts the year from the url of an iGEM wiki page
+    
+    Arguments:
+        url (str): the url of a wiki page
+
+    Returns:
+        year (str): the year of competition for the team 
+    '''
+    
+    year = re.search('//(.+?).igem', url).group()   # Extracts the year from the URL
+    year = year.replace('//', '')                   # Removes the '//'
+    year = year.replace('.igem', '')                # Removes the '.igem'
+
+    return year
+
+def cleanHTML(page_html):
+    '''
+    Processes and cleans the HTML from the scraped page, removing any Javascript, style, or scripts
+    
+    Arguments:
+        page_html (str): the full HTML content of the scraped page
+
+    Returns:
+        clean_page (str): the processed version of the page with JS, style, and scripts removed 
+    '''
+    
+    page = html.fromstring(page_html)   # Turns the HTML string into an lxml HTML Element in order to use the Cleaner object
+
+    cleaner = Cleaner(style=True, javascript=True, scripts=True)   # Creates an lxml Cleaner class to remove JS, styles, and scripts
+
+    clean_page = cleaner.clean_html(page)   # Cleans the page according to the parameters above
+
+    tags_to_remove = ['nav', 'footer', 'header', 'h1', 'h2', 'h3']
+    for tag in tags_to_remove:  # Adapted from https://stackoverflow.com/a/63386884
+        for node in clean_page.xpath(f'.//{tag}'):
+            node.getparent().remove(node)
+
+    clean_page = html.tostring(clean_page, encoding='UTF-8').decode()    # Converts it back into a string in order to use it later on
+
+    return clean_page
+    
+def getPagetext(clean_page):
+    '''
+    Gets the text content from the body of a wiki page and cleans it from
+    newlines, tabs, and excessive whitespace
+
+    Arguments:
+        clean_page (str): the processed HTML version of the page (see cleanHTML)
+
+    Returns:
+        pagetext (str): the raw text from the body of a wiki page
+    '''
+
+    selector = Selector(text=clean_page)    # Creates a Selector class to use CSS selectors (scrapy can do some by default but it was buggy)
+    page_body = selector.css('div#bodyContent *::text').getall()    # Gets a list of divs with the id "bodyContent". Most iGEM pages only have one div
+
+    pagetext = ' '.join(str(div) for div in page_body)    # Turns the text in bodyContent into a string
+
+    
+    
+    pagetext = pagetext.replace('"', "'")   # Replaces the '"' characters with ''' in order to avoid premature closing of the string in the export file.
+
+
+    chars_to_remove = [r'\\n', r'\n', r'\t', r'\\t']    # List of newline and tab chars that I observed during testing that needed to be removed
+
+    for char in chars_to_remove:    # Iterates through chars_to_remove and removes the chars from the pagetext
+        pagetext = pagetext.replace(char, '')
+
+    pagetext = ' '.join(pagetext.split())  # Cleans weird spacing and newlines 
+    
+    return pagetext
+
+class iGEMSpider(CrawlSpider):
+    '''
+    A scrapy CrawlSpider that will automatically folow every link it finds on a page,
+    starting with the 2021 iGEM teamlist. If the link matches certain rules, it will 
+    automatically parse it, scrape it, process it, and output it to the file you select.
+    
+    Inputs:
+        start_urls [list of str]: the starting url for the scraper to begin
+
+    Yields:
+        dictionary -> file (see initial documentation):
+            url (str): the url of the software/modelling page
+            pagetype (str): a string identifying if the page is a software or modelling page
+                            in order to help filter them down the line
+            teamname (str): identifies the team that created the page (see getTeamname)
+            year (str): identifies the year of competition of the team (see getYear)
+            pagetext (str): the body content of the wiki page (see getPagetext)
+
+    '''
+    name = 'tomholland'     # I'm a comedian
+    allowed_domains = ['igem.org']  # You need an allowed domains list to get the CrawlScraper to work and I'd rather not have the bot scrape the entirety of the internet.
+    rules = (
+        
+        # First rule looks for pages with the word Mode + following characters in the URL in order to get pages with Model, Modelling, and Modelling. The pages that match the criteria get callbacked to parse_model_page
+        Rule(LinkExtractor(allow=(r'Mode\w+')), callback='parse_model_page'), 
+        
+        # Second rule looks for pages with the word /Software in the URL in order to to avoid teams with software in the name. The pages that match the criteria get callbacked to parse_soft_page
+        Rule(LinkExtractor(allow=(r'/Software')), callback='parse_soft_page'),
+
+        # Final rule looks for pages with the word Team: in the URL, while denying pages that got the crawler in an infinite loop. There is no callback, allowing the crawler to use these pages to find new links.
+        # We've also excluded pages that we don't want to scrape (to decrease scaping times). If modifying, ensure that the pages you want aren't included in this list.
+        # Theoretically, there is no issue with keeping it if you've created a rule to get the pages above because the rules are checked top-down, but I wouldn't personally risk it.
+        Rule(LinkExtractor(allow=('Team:', ), deny=('oldid=', 'Login', 'action=history', 'wiki', 'Special', 'Journal', 'Notebook', r'Protocol\w+', 'Safety', 'Results', 'Parts', 'Description', 'Judging', r'Sustainab\w+', 'Sponsers', 'Partnership', 'Education'))),
+        
+
+    )
+
+    custom_settings = {
+        
+        # This is 30 to allow much faster scraping. It was 100 once, but the iGEM servers didn't like that 
+        # and nearly banned me so the theoretical maximum limit is somewhere in between.
+        'CONCURRENT_REQUESTS_PER_DOMAIN' : 30,
+        'CONCURRENT_REQUESTS' : 60
+    }
+
+    start_urls = [
+        'https://old.igem.org/Team_List?year=2021&name=Championship&division=igem'
+    ]
+
+    def parse_model_page(self, response):
+        '''
+        Function called when a page matches rule 1 of the CrawlSpider rules
+        Parses the model page.
+        
+        Arguments:
+            response (Response): the response given by the Scrapy request
+
+        Yields:
+            dictionary -> file (see initial documentation):
+                url (str): the url of the software/modelling page
+                pagetype (str): a string identifying if the page is a software or modelling page
+                                in order to help filter them down the line
+                teamname (str): identifies the team that created the page (see getTeamname)
+                year (str): identifies the year of competition of the team (see getYear)
+                pagetext (str): the body content of the wiki page (see getPagetext)
+        '''
+        
+        url = str(response.url)
+        teamname = getTeamname(url)
+        year = getYear(url)
+        clean_html = cleanHTML(str(response.text))
+        pagetext = getPagetext(clean_html)
+
+        yield {
+            'url' : url,
+            'pagetype' : 'model',
+            'teamname' : teamname,
+            'year' : year,
+            'pagetext' : pagetext,
+        }
+    def parse_soft_page(self, response):
+        '''
+        Function called when a page matches rule 2 of the CrawlSpider rules
+        Parses the software page
+        
+        Arguments:
+            response (Response): the response given by the Scrapy request
+
+        Yields:
+            dictionary -> file (see initial documentation):
+                url (str): the url of the software/modelling page
+                pagetype (str): a string identifying if the page is a software or modelling page
+                                in order to help filter them down the line
+                teamname (str): identifies the team that created the page (see getTeamname)
+                year (str): identifies the year of competition of the team (see getYear)
+                pagetext (str): the body content of the wiki page (see getPagetext)
+        '''
+        
+        url = str(response.url)
+        teamname = getTeamname(url)                 # Calls the getTeamname function
+        year = getYear(url)
+        clean_html = cleanHTML(str(response.text))
+        pagetext = getPagetext(clean_html)
+
+        yield {
+            'url' : url,
+            'pagetype' : 'software',
+            'teamname' : teamname,
+            'year' : year,
+            'pagetext' : pagetext,
+        }
+
+
