fb93fc1fbe2ee1f37e118be7a921c081218083e6
EbunMak <ebunoluwa.makinde@ucalgary.ca>
2022-06-15 14:59:04 -0600

    Added Summarizer

--- netscrape_nav/netscrape_nav/pipelines.py
+++ netscrape_nav/netscrape_nav/pipelines.py
@@ -18,8 +18,60 @@ Doesn't run on it's own; is accessed from iGEMScraper.py when running the comman
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
 from scrapy.exporters import JsonLinesItemExporter
+# import and initialize the tokenizer and model from the checkpoint
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import re
+import nltk
 
+
+nltk.download('punkt')
+checkpoint = "sshleifer/distilbart-cnn-12-6"
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+
+#Summarizer using the 'distilbart-cnn-12-6' pretrained model
+def generate_summary(text):
+  # tokenize the text into sentences
+  sentences = nltk.tokenize.sent_tokenize(text)
+  # initialize
+  length = 0
+  chunk = ""
+  chunks = []
+  count = -1
+  for sentence in sentences:
+    count += 1
+    combined_length = len(tokenizer.tokenize(sentence)) + length # add the no. of sentence tokens to the length counter
+
+    if combined_length  <= tokenizer.max_len_single_sentence: # if it doesn't exceed
+      chunk += sentence + " " # add the sentence to the chunk
+      length = combined_length # update the length counter
+
+      # if it is the last sentence
+      if count == len(sentences) - 1:
+        chunks.append(chunk.strip()) # save the chunk
+      
+    else: 
+      chunks.append(chunk.strip()) # save the chunk
+      
+      # reset 
+      length = 0 
+      chunk = ""
+
+      # take care of the overflow sentence
+      chunk += sentence + " "
+      length = len(tokenizer.tokenize(sentence))
+  len(chunks)
+  # inputs to the model
+  inputs = [tokenizer(chunk, return_tensors="pt") for chunk in chunks]
+  compiled_summary = ''
+  #for every chunk of the tokenized text
+  for input in inputs:
+    #generate the summary
+    output = model.generate(**input)
+    #add each chunk of summary to a string
+    compiled_summary = compiled_summary + tokenizer.decode(*output, skip_special_tokens=True)
+  return compiled_summary   
 
 class KeystoneXL:
     '''
@@ -59,6 +111,7 @@ class KeystoneXL:
         
         
         scraped_data['pagetext'] = re.sub(r'\$\$.+?\$\$', '', scraped_data['pagetext']) # Removes equations from the exported data
+        scraped_data['pagetext'] = generate_summary(scraped_data['pagetext'])
         
         self.exporter.export_item(item) # Exports the item to the file specified
         return item 
